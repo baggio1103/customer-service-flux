0f797c6640465c7550a912847bd373b315d738a3
baggio1103 <valijanov1103@mail.ru>
2022-12-12 14:14:52 +0300

    Write pritive examples using Mono<T> and Flux<T>

---
+++ src/test/java/com/javajedi/MonoFluxTest.java
@@ -0,0 +1,27 @@
+package com.javajedi;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class MonoFluxTest {
+
+    @Test
+    void testMono() {
+        var mono = Mono.just("Java-jedi")
+                .then(Mono.error(new RuntimeException("Error in Flux")))
+                        .log();
+        mono.subscribe(System.out::println, e -> System.out.println(e.getMessage()), () -> System.out.println("Success"));
+    }
+
+    @Test
+    void testFlux() {
+        var flux = Flux.just("Java", "Spring", "PostgreSQL")
+                .concatWithValues("AWS")
+                .concatWith(Flux.error(new RuntimeException("Exception happened in Flux")))
+                .concatWithValues("Cloud")
+                .log();
+        flux.subscribe(System.out::println, e -> System.out.println(e.getMessage()));
+    }
+
+}
